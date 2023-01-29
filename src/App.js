3e4ef32612fa6803c2f610bc54fbcded5100d29d
brian <mairurat@gmail.com>
2023-01-30 00:32:16 +0300

    Jsonapi

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import "./App.css";
-const axios = require("axios");
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const options = {
-    method: "GET",
-    url: "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/direct/",
-    params: { destination: "LED", origin: "MOW" },
-    headers: {
-      "X-Access-Token": "undefined",
-      "X-RapidAPI-Key": "4c31cf640cmshdfbdc0851576d6bp16ec33jsn6339f47a9bff",
-      "X-RapidAPI-Host":
-        "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
-    },
-  };
+  const [data, setData] = useState();
 
-  axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+  async function getData() {
+    await axios
+      .get("https://jsonplaceholder.typicode.com/posts/1/comments")
+      .then((response) => {
+        let res = response.data;
+        setData(res);
+        console.log("Results:", res);
+      });
+  }
 
   return (
     <div className="App">
-      <button onClick={options}>Click Me</button>
+      <h3>Results</h3>
+      <div>
+        {data.map((item, i) => (
+          <div key={i}>
+            <p>{item}</p>
+          </div>
+        ))}
+        <button onClick={getData}>Click Me</button>
+      </div>
     </div>
   );
 }
