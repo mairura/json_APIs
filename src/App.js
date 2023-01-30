4e8000a77a92ea38e53f33fdf4cfd0148af8a3d7
brian <mairurat@gmail.com>
2023-01-30 14:13:01 +0300

    Accesses all items in an array

--- src/App.js
+++ src/App.js
@@ -3,30 +3,50 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
 
-  async function getData() {
-    await axios
-      .get("https://jsonplaceholder.typicode.com/posts/1/comments")
-      .then((response) => {
-        let res = response.data;
-        setData(res);
-        console.log("Results:", res);
-      });
-  }
+  useEffect(() => {
+    async function getData() {
+      try {
+        const data = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts/1/comments"
+        );
+        let apiData = data.data;
+        console.log("Print Data:", apiData);
+
+        const myData = await Promise.all(
+          apiData.map((item) => {
+            const k = item;
+            console.log("Value K:", k);
+
+            const itemData = {
+              name: item.name,
+            };
+            return itemData;
+          })
+        );
+
+        setData(myData);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    getData();
+  }, []);
+
+  console.log("Print Data items:", data);
 
   return (
-    <div className="App">
-      <h3>Results</h3>
-      <div>
-        {data.map((item, i) => (
-          <div key={i}>
-            <p>{item}</p>
-          </div>
-        ))}
-        <button onClick={getData}>Click Me</button>
+    <>
+      <div className="App">
+        <h3>Results</h3>
+        <div className="items_data">
+          {data.map((item, index) => (
+            <div>{item.name}</div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
